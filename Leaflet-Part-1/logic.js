bfdf135731fb3903cb28fa16c6b9199519a2c452
Groves <Groves.Bryan@mayo.edu>
2022-11-16 20:45:28 -0600

    added code

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -1,69 +1,88 @@
-// Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Creating the map object
+let myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 4
+});
 
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  console.log(data); 
-  createFeatures(data.features);
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Use this link to get the GeoJSON data.
+let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// The function that will determine the color of a neighborhood based on the borough that it belongs to
+function getColor(depth) {
+  if (depth < 10) return "#a3f600";
+  else if (depth == "Bronx") return "#dcf400";
+  else if (depth < 50) return "#f7db11";
+  else if (depth == 70) return "#fdb72a";
+  else if (depth == 90) return "#fca35d";
+  else return "#ff5f65";
+  
+  //return "";
+}
+
+function getRadius(magnitude) {
+ 
+  if (magnitude == 0) 
+    {return 1};
+
+  return magnitude * 2;
+
+}
+
+function styleInfo(feature){
+
+  // console.log(feature.geometry);
+
+
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.geometry.coordinates[2]),
+    color: "black",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+
+  };
+
+}
+
+
+// Getting our GeoJSON data
+d3.json(link).then(function(data) {
+
+  console.log(data);
+
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Styling each feature (in this case, a neighborhood)
+    style: styleInfo,
+
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+
+
+    // This is called on each feature.
+    onEachFeature: function(feature, layer) {
+     
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h1>" + feature.properties.mag + "</h1> <hr> <h2>" + feature.geometry.coordinates + "</h2>");
+
+    }
+
+    
+  }).addTo(myMap);
 });
 
 
-function createFeatures(earthquakeData) {
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.mag}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    Earthquakes: earthquakes
-  };
-  // Define a markerSize() function that will give each city a different radius based on its population.
-  function markerSize(mag) {
-    return Math.sqrt(mag) * 50;
-}
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
+/////TODO
+  //Center map
+  //If then logic in get color function to color based on depth 
+  //popups
